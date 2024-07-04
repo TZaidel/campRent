930abad00a43b49c989a0af2b72e4e9623b24f77
Tatiana Zaidel <8tzaidel@gmail.com>
2024-07-04 16:34:17 +0200

    edit img routes

--- src/components/Hero/images.js
+++ src/components/Hero/images.js
@@ -1,34 +1,34 @@
 export const images = [
   {
     id: 101,
-    imgUrl: '../../../public/img/F21_5206.jpg',
+    imgUrl: '/img/F21_5206.jpg',
   },
   {
     id: 102,
-    imgUrl: '../../../public/img/F21_5208.jpg',
+    imgUrl: '/img/F21_5208.jpg',
   },
   {
     id: 103,
-    imgUrl: '../../../public/img/F21_5274.jpg',
+    imgUrl: '/img/F21_5274.jpg',
   },
   {
     id: 104,
-    imgUrl: '../../../public/img/F21_5304.jpg',
+    imgUrl: '/img/F21_5304.jpg',
   },
   {
     id: 105,
-    imgUrl: '../../../public/img/F21_5350.JPG',
+    imgUrl: '/img/F21_5350.JPG',
   },
   {
     id: 106,
-    imgUrl: '../../../public/img/F21_5467.jpg',
+    imgUrl: '/img/F21_5467.jpg',
   },
   {
     id: 107,
-    imgUrl: '../../../public/img/F21_5577.jpg',
+    imgUrl: '/img/F21_5577.jpg',
   },
   {
     id: 108,
-    imgUrl: '../../../public/img/F21_5769.jpg',
+    imgUrl: '/img/F21_5769.jpg',
   },
 ];
